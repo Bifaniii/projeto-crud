16c61cb933d6514f42211b44babd6becd544b9fb
Bifaniii <gbifani.tech@gmail.com>
2025-11-07 01:28:52 -0300

    Initial commit

---
+++ src/main/java/com/bifani/projeto_crud/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.bifani.projeto_crud.infrastructure.repository;
+
+import com.bifani.projeto_crud.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
